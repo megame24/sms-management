93454c201dac3f15bfe5dbc55cb254b454186c2e
Innocent Ngene <ngeneinnocent@gmail.com>
2019-08-09 10:53:42 +0100

    send sms

---
+++ middleware/validation/smsValidator.js
@@ -0,0 +1,39 @@
+const { Contact } = require('../../database/models');
+
+/* eslint-disable max-len */
+/* eslint-disable no-useless-escape */
+const { throwError } = require('../../helpers/errorHelper');
+
+/**
+ * SmsValidator constructor
+ * @returns {undefined}
+ */
+function SmsValidator() {}
+
+/**
+ * Validate sending sms
+ * @param {Object} req request object
+ * @param {Object} res response object
+ * @param {Function} next next function in the
+ * middleware chain
+ * @returns {Function} next
+ */
+SmsValidator.prototype.validateSend = async (req, res, next) => {
+  let { message, recipientNumber } = req.body;
+  const { contact: { id } } = req;
+  message = message && message.trim() ? message.trim() : null;
+  recipientNumber = recipientNumber && recipientNumber.trim() ? recipientNumber.trim() : null;
+  try {
+    if (!message) throwError('The message field is required', 400);
+    if (!recipientNumber) throwError('The recipientNumber field is required', 400);
+    const recipient = await Contact.findOne({ where: { phoneNumber: recipientNumber } });
+    if (!recipient || recipient.id === id) throwError('Recipient not found', 400);
+    req.body.recipient = recipient;
+    req.body = { ...req.body, message, recipientNumber };
+    return next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = new SmsValidator();
